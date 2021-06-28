bc6b3250e5f8814e4a1ce84aae2eb229dc09b3c5
nhuse <nathan.huse@yahoo.com>
2021-06-28 12:22:37 -0500

    commit tree f3144884ffa7dd1f682a1d5bb119d73aa611caeb

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!'
+  }
+  else if(feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if(feet > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response;
+  city === 'NYC' ? (response = 'Ok, sounds good.') : (response = 'No go.');
+  return response;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generousity){
   // Write your code here!
+  let response;
+  switch(generousity) {
+    case 'not as generous':
+      response = 'Thank you.'
+      break;
+    case 'generous':
+      response = 'Thank you so much.'
+      break;
+    default:
+      response = 'Bye.'
+      break;
+  }
+  return response;
 }
